6e78fd7adc3a578057c96e9d835e7057fd317c7c
刘 喜东 <liuxidong1984@gmail.com>
2014-08-13 10:00:17 +0800

    improve printing

--- cmd/merge-one.sh
+++ cmd/merge-one.sh
@@ -4,21 +4,74 @@
 # Demo:
 #     merge-one.sh app-render
 
-code_path=/Users/liuxd/Documents/git.ipo.com/hf-code/
+# Colorful printing.
+# Demo:
+#     _cecho 'hello' error
+function _cecho {
+    case $2 in
+        info )
+            color=33;;
+        error )
+            color=31;;
+        success )
+            color=32;;
+        *)
+            color=1;;
+    esac
+
+    echo -e "\033["$color"m$1\033[0m"
+}
+
+# Merge develop to master.
+# @param string the path of repository.
+# @param string the path of git.
+# Demo:
+#     _merge /home/www/code/hf-index/ /bin/git
+function _merge {
+    cd $1
+    _cecho "$2 clean -df" success
+    $2 clean -df
+
+    _cecho "$2 reset --hard" success
+    $2 reset --hard
+
+    _cecho "$2 checkout master" success
+    $2 checkout master
+
+    _cecho "$2 pull origin master" success
+    $2 pull origin master
+
+    _cecho "$2 checkout develop" success
+    $2 checkout develop
+
+    _cecho "$2 pull origin develop" success
+    $2 pull origin develop
+
+    _cecho "$2 checkout master" success
+    $2 checkout master
+
+    _cecho "$2 merge develop --no-ff -m 'merge develop'" success
+    $2 merge develop --no-ff -m 'merge develop'
+
+    result=$?
+
+    if [ $result == 0 ];then
+        $2 push origin master
+    fi   
+}
+
+#code_path=/Users/liuxd/Documents/git.ipo.com/hf-code/
+code_path=/Users/liuxd/Documents/git.ipo.com/
 repo=$1
 git=/usr/local/git/bin/git
 
-cd $code_path$repo
-$git clean -df
-$git reset --hard
-$git checkout master
-$git pull origin master
-$git checkout develop
-$git pull origin develop
-$git checkout master
-$git merge develop --no-ff -m 'merge develop'
-result=$?
+repo_path=$code_path$repo
 
-if [ $result == 0 ];then
-    $git push origin master
+if [ ! -d $repo_path ];then
+    echo No such folder : $repo_path
+    exit
 fi
+
+_merge $repo_path $git
+
+# end of this file
