8f6bf49bbdadc12b04e0db49553a4e825f3f2728
刘 喜东 <liuxidong1984@gmail.com>
2014-08-15 11:17:55 +0800

    websocket demo

--- socket/server.js
+++ socket/server.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+app.get('/', function(req, res) {
+    var str = fs.realpathSync('.');
+    res.sendFile(str + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat', function(data) {
+        socket.emit('chat', data);
+        console.log(data);
+    });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
