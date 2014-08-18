a68f09f177f98f4772a6868428e75bea77a1abf2
刘 喜东 <liuxidong1984@gmail.com>
2014-08-18 11:01:24 +0800

    for page

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+var port = 3000;
+app.listen(port, function() {
+    console.log('listening on *:' + port);
 });
