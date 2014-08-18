1096a9680ace4687e3de47fef7e7bc87c70afe3a
刘 喜东 <liuxidong1984@gmail.com>
2014-08-18 14:21:20 +0800

    add socket.io client

--- server/www/application/js/home.js
+++ server/www/application/js/home.js
@@ -1,25 +1,25 @@
 (function() {
-//    var socket = io();
-//    var channel = 'cli';
-//    $('#send').click(function() {
-//        var msg = $('#m').val();
-//        socket.emit(channel, {msg: msg});
-//        $('#m').val('');
-//        return false;
-//    });
-//    socket.on(channel, function(msg) {
-//        console.log(msg);
-//        $('#messages').append($('<li>').text(msg.msg));
-//    });
+    var socket = io();
+    var channel = 'cli';
+    $('#send').click(function() {
+        var msg = $('#m').val();
+        socket.emit(channel, {msg: msg});
+        $('#m').val('');
+        return false;
+    });
+    socket.on(channel, function(msg) {
+        console.log(msg);
+        $('#messages').append($('<li>').text(msg.msg));
+    });
 
-    $.get('/get_repo_list', function(data, status){
+    $.get('/get_repo_list', function(data, status) {
         if (status === 'success') {
             var repos = eval('(' + data + ')').repos;
             var repo_container = $('#repo_list');
 
             for (i in repos) {
                 var repo = repos[i];
-                var html = "<span><label class='checkbox-inline repo'><input type='checkbox' value='" + repo +"'>" + repo +"</label></span>";
+                var html = "<span><label class='checkbox-inline repo'><input type='checkbox' value='" + repo + "'>" + repo + "</label></span>";
                 $(html).appendTo(repo_container);
             }
         }
